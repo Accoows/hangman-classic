2c093dda00dd7a92b459c15794e6bf387b7814d8
Accoow <gnzlsarthur@gmail.com>
2024-09-23 11:39:51 +0200

    Import Read File

--- hangman.go
+++ hangman.go
@@ -1,11 +1,28 @@
 package hangmanclassic
 
-/*import (
-	"fmt"
-	"io/ioutil"
-	"math/rand"
-)*/
+import (
+	/*"io/ioutil"
+	"math/rand"*/
+	"bufio"
+	"os"
+)
 
 var maxtentative int = 10
 
-func readWordsF() {}
+func readWordsFromFile(filename string) ([]string, error) {
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var words []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	return words, scanner.Err()
+}
